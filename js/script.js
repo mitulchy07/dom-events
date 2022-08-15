8c7727d18d8a12ca4a4b13fa22fa6c5e6cf674f6
mitulchy07 <mitul9616@gmail.com>
2022-08-15 19:24:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('button-submit').addEventListener('click',function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passField = document.getElementById('user-pass');
+    const password = passField.value;
+    if(email === 'hasan@gmail.com' && password === '12345'){
+        window.location.href = 'bank.html'
+    }
+    else{
+       alert('Warning!!! Invalid User.');
+    }
+     
+})
