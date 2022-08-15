22b75daa1c08d7748985ccb38834f6816d0ad8e7
mitulchy07 <mitul9616@gmail.com>
2022-08-15 23:05:22 +0200

    project done

--- js/bank.js
+++ js/bank.js
@@ -1,17 +1,49 @@
-
+   const balance = document.getElementById('balance-display');
+    const finalBalance = balance.innerText;
+    const displayBalance = parseFloat(finalBalance);
+    console.log(displayBalance);
 
 
 document.getElementById('deposite-button').addEventListener('click', function(){
     const depositeInput = document.getElementById('deposite-input');
     const deposite = depositeInput.value;
+    const newDeposite = parseFloat(deposite);
     const depositeDisplay = document.getElementById('deposite-display');
-    depositeDisplay.innerText = deposite;
+    const newDepositeDisplay = depositeDisplay.innerText;
+    const currentDepositeDisplay = parseFloat(newDepositeDisplay);
+    const totalDeposite = newDeposite + currentDepositeDisplay;
+    depositeDisplay.innerText = totalDeposite;
+    balance.innerText = totalDeposite + displayBalance; 
 })
+
+
+
+
 
 
 document.getElementById('withdraw-button').addEventListener('click', function(){
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdraw = withdrawInput.value;
-    const withdrawDisplay = document.getElementById('withdraw-display');
-    withdrawDisplay.innerText = withdraw;
+     const withdrawInput = document.getElementById('withdraw-input');
+    const withdraw = withdrawInput.value; 
+    const newWithdraw = parseFloat(withdraw); 
+    if(displayBalance < newWithdraw){
+        alert('Balance Low!!!');
+        return;
+
+    }
+    
+     const withdrawDisplay = document.getElementById('withdraw-display');
+    const newWithdrawDisplay = withdrawDisplay.innerText;
+    const currentWithdrawDisplay = parseFloat(newWithdrawDisplay);
+    const totalWithdraw = newWithdraw + currentWithdrawDisplay;
+    withdrawDisplay.innerText = totalWithdraw;
+    balance.innerText = displayBalance - totalWithdraw; 
+ 
+   
+
+    
 })
+
+
+
+
+
